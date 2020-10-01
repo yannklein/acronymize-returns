7434b12bf52d49af8e41b3b167aa1203f53ab8d1
Yann Klein <yann.klein@me.com>
2020-10-01 19:46:52 +0900

    acro base with pseudo code

--- acronymize.rb
+++ acronymize.rb
@@ -1,11 +1,10 @@
 def acronymize(sentence)
-  words = sentence.split(" ")
-  first_letters = []
-  words.each do |word|
-    first_letters << word[0]
-  end
-  first_letters.join.upcase
-end
+  # 1. Split the sentence into words
+  # 2. Initialize an empty array A
+  # 3. Iterate over words, for each word, select the first letter (how?)
+  #    and store it in A
+  # 4. Join elements of A, uppercase it and return it
+ends
 
 puts acronymize("Frequently Asked Questions") == "FAQ"
 puts acronymize("") == ""
